686f90f8fe69439a06638e30d5b029b90d5e8432
gigann <issacgann@gmail.com>
2025-03-27 20:29:49 +0100

    searching

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -15,13 +17,39 @@ function App() {
       })
   }, []);
 
+  useEffect(() => {
+    fetch(`http://localhost:3000/movies/search/${search}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSearchResults(data);
+        console.log(data)
+      })
+  }, [search]);
+
   return (
     <>
       <h1>Movie Data</h1>
 
+      <h2>All Movie Titles</h2>
       <ul>
         {movies.map(movie => <li key={movie.title}>{movie.title}</li>)}
       </ul>
+
+      <form>
+        <input type='text' id='movie-search' placeholder='Search for a movie' onChange={() => {
+          setSearch(document.querySelector('#movie-search').value);
+        }}></input>
+      </form>
+
+      <h2>Search Results</h2>
+      <ul>
+        {(searchResults.length > 0) ? (
+          searchResults.map(movie => <li key={movie.title}>{movie.title}</li>)
+        ):(
+        null
+        )}
+      </ul>
+
     </>
   )
 }
