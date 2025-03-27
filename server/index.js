8b545ed3b6c1bb8352af3521758490536fc04150
gigann <issacgann@gmail.com>
2025-03-27 20:29:30 +0100

    searching

--- server/index.js
+++ server/index.js
@@ -10,22 +10,31 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+
 // GET
 app.get('/movies', (req, res) => {
-    knex('movies')
+    return knex('movies')
         .select('*')
         .then(data => res.status(200).json(data))
         .catch(err => res.status(400).json({ message: 'All Movie data could not be found.' }))
 })
 app.get('/movies/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    knex('movies')
+    return knex('movies')
         .select('*')
         .where('id', id)
         .then(data => res.status(200).json(data))
         .catch(err => res.status(400).json({ message: 'Movie id could not be found.' }))
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+// search
+
+app.get('/movies/search/:search', (req, res) => {
+    const search = req.params.search;
+    return knex('movies')
+        .select('*')
+        .whereILike('title', `%${search}%`)
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(400).json({ message: 'Movie title could not be found.' }))
 })
